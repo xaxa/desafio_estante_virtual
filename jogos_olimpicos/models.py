f02edfc69206038117b81e37f55948be04001144
Vinicius Xaxá <vinicius.xaxa@gmail.com>
2018-05-12 00:31:32 -0300

    model basica

--- jogos_olimpicos/models.py
+++ jogos_olimpicos/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
-# Create your models here.
+
+class Atleta(models.Model):
+    criacao = models.DateTimeField(auto_now_add=True)
+    nome = models.CharField(max_length=150, blank=False)
+
+    def __str__(self):
+        return self.nome
+
+
+class Competicao(models.Model):
+    criacao = models.DateTimeField(auto_now_add=True)
+    nome = models.CharField(max_length=150, blank=False) 
+    quantidade_chances = models.SmallIntegerField( blank=False)
+    finalizada = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.nome
+
+    
+
+class Resultado(models.Model):
+    criacao = models.DateTimeField(auto_now_add=True)
+    competicao = models.ForeignKey(Competicao, on_delete=models.CASCADE,null=False, blank=False)
+    atleta = models.ForeignKey(Atleta, on_delete=models.CASCADE,null=False, blank=False)
+    valor = models.DecimalField(max_digits=6, decimal_places=3, blank=False)
+    unidade = models.CharField(choices=UNIDADES,max_length=1,blank=False)
+
+    class Meta:
+        ordering = ('valor',)
+
+
+
