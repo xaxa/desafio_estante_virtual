d3062d3b2121b9b4fb7179192d4a296b5559728c
Vinicius Xaxá <vinicius.xaxa@gmail.com>
2018-05-12 10:31:41 -0300

    Models

--- jogos_olimpicos/models.py
+++ jogos_olimpicos/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.db.models.signals import pre_save
+from rest_framework.exceptions import APIException
 UNIDADES = (
 		('s','Segundos'),
 		('m','Metros'),
@@ -7,7 +9,6 @@ CRITERIO_VENCEDOR = (
         ('maior','Maior valor'),
         ('menor','Menor valor'),
     )
-
 class Atleta(models.Model):
     criacao = models.DateTimeField(auto_now_add=True)
     nome = models.CharField(max_length=150, blank=False)
@@ -26,7 +27,12 @@ class Competicao(models.Model):
     def __str__(self):
         return self.nome
 
-    
+    @staticmethod
+    def validar_finalizada(sender, instance, **kwargs):
+        if instance.id:
+            competicao = Competicao.objects.get(pk=instance.pk)
+            if competicao.finalizada:
+                raise APIException("Competição finalizada")
 
 class Resultado(models.Model):
     criacao = models.DateTimeField(auto_now_add=True)
@@ -38,5 +44,23 @@ class Resultado(models.Model):
     class Meta:
         ordering = ('valor',)
 
+    @staticmethod
+    def validar_quantidade_participacao(sender, instance, **kwargs):
+        competicao = Competicao.objects.get(pk=instance.competicao_id)
+        atleta = Atleta.objects.get(pk=instance.atleta_id)
+        total_resultados_atleta = Resultado.objects.filter(competicao=competicao,atleta=atleta).count()
+        if total_resultados_atleta >= competicao.quantidade_chances:
+            raise APIException("Atleta já teve quantidade de partipação máxima.")
+
+    @staticmethod
+    def validar_competicao_finalizada(sender, instance, **kwargs):
+        competicao = Competicao.objects.get(pk=instance.competicao_id)
+        if competicao.finalizada:
+            raise APIException("Competição finalizada")
+
+
+pre_save.connect(Competicao.validar_finalizada,Competicao)
+pre_save.connect(Resultado.validar_competicao_finalizada,Resultado)
+pre_save.connect(Resultado.validar_quantidade_participacao,Resultado)
 
 
