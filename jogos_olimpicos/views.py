f3a406a9d5a5e81f07a4788679f0df64c6fc2b81
Vinicius Xaxá <vinicius.xaxa@gmail.com>
2018-05-12 00:45:00 -0300

    view basica

--- jogos_olimpicos/views.py
+++ jogos_olimpicos/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from jogos_olimpicos.models import Atleta,Competicao,Resultado
+from jogos_olimpicos.serializers import AtletaSerializer,CompeticaoSerializer,ResultadoSerializer
+from rest_framework import viewsets
 
-# Create your views here.
+
+class AtletaViewSet(viewsets.ModelViewSet):
+    queryset = Atleta.objects.all()
+    serializer_class = AtletaSerializer
+
+class CompeticaoViewSet(viewsets.ModelViewSet):
+    queryset = Competicao.objects.all()
+    serializer_class = CompeticaoSerializer
+
+
+
+class ResultadoViewSet(viewsets.ModelViewSet):
+    queryset = Resultado.objects.all()
+    serializer_class = ResultadoSerializer
+
+
+
